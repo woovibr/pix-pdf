29813c0f9c39c8c09066e95c72cd0951e5f02c9f
Julio Merisio <contatomerisio@gmail.com>
2024-04-25 21:54:44 -0300

    refactor: update schema

--- src/components/qr-code-svg.tsx
+++ src/components/qr-code-svg.tsx
@@ -1,18 +1,21 @@
-import React from 'react'
+import React from "react";
 import QR from "qrcode.react";
-import {Path, Svg} from "@react-pdf/renderer";
+import { Path, Svg } from "@react-pdf/renderer";
+import { parseHtmlSvgToPdf } from "../utils/parseHtmlSvgToPdf";
+import { renderToString } from "react-dom/server";
 
-export const createQRCodeSVG =  (url: string) => {
-    //@ts-expect-error qrcode-react does not have a type definition
-    return <QR.QRCodeSVG value={url} size={128} />;
-}
+export const createQRCodeSVG = (url: string) => {
+  //@ts-expect-error qrcode-react does not have a type definition
+  return <QR.QRCodeSVG value={url} size={128} />;
+};
 
-export const QRCode = ({qrcode}: { qrcode: string[] }) => {
-    return (
-        <Svg height="128" width="128" viewBox="0 0 41 41">
-            {qrcode.map((path, index) => (
-                <Path key={path} fill={index === 0 ? 'white' : 'black'} d={path}/>
-            ))}
-        </Svg>
-    );
+export const QRCode = ({ qrcode }: { qrcode: string }) => {
+  const paths = parseHtmlSvgToPdf(renderToString(createQRCodeSVG(qrcode)));
+  return (
+    <Svg height="200" width="200" viewBox="0 0 100 100">
+      {paths.map((path, index) => (
+        <Path key={path} fill={index === 0 ? "white" : "black"} d={path} />
+      ))}
+    </Svg>
+  );
 };
--- src/templates/Boleto.tsx
+++ src/templates/Boleto.tsx
@@ -1,21 +1,69 @@
 import React from "react";
-import {Document, Page, StyleSheet, Text, View,} from "@react-pdf/renderer";
+import {
+  Document,
+  Link,
+  Page,
+  StyleSheet,
+  Text,
+  View,
+} from "@react-pdf/renderer";
+import { createPdf } from "../factories";
+import { z } from "zod";
+import { boletoBarcodeSvg } from "../components/barcode";
+import { WooviLogo } from "../components/woovi-logo";
+import { QRCode } from "../components/qr-code-svg";
 
-import {createPdf} from "../factories";
-import {z} from "zod";
-import {boletoBarcodeSvg} from "../components/barcode";
-import {WooviLogo} from "../components/woovi";
-import VercelLogo from "../components/vercel";
-import {createQRCodeSVG, QRCode} from "../components/qr-code-svg";
-import {renderToString} from "react-dom/server";
-import {parseHtmlSvgToPdf} from "../utils/parseHtmlSvgToPdf";
-
-const inputSchema = z.object({
-  companyName: z.literal("Woovi").or(z.literal("Vercel")),
-  number: z.string(),
-  short: z.string(),
-  dueDate: z.string(),
-  qrcode: z.string(),
+export const inputSchema = z.object({
+  barcodeData: z.string(),
+  scannerBarcode: z.string(),
+  qrCodeData: z.string(),
+  paymentPlace: z.string(),
+  beneficiary: z.string(),
+  beneficiaryAddress: z.string(),
+  instructions: z.string(),
+  agency: z.string(),
+  agencyDigit: z.string(),
+  account: z.string(),
+  accountDigit: z.string(),
+  expirationDay: z.date(),
+  documentDate: z.date(),
+  processingDate: z.date(),
+  card: z.string(),
+  documentNumber: z.string(),
+  formattedOurNumber: z.string(),
+  formattedValue: z.string(),
+  documentType: z.string(),
+  accept: z.string(),
+  currencyType: z.string(),
+  amount: z.string(),
+  valueOf: z.string(),
+  discountValue: z.string(),
+  otherDiscounts: z.string(),
+  feeValue: z.string(),
+  otherFees: z.string(),
+  chargeValue: z.string(),
+  payer: z.object({
+    name: z.string(),
+    registerNumber: z.string(),
+    street: z.string(),
+    number: z.string(),
+    complement: z.string(),
+    district: z.string(),
+    city: z.string(),
+    state: z.string(),
+    postalCode: z.string(),
+  }),
+  guarantor: z.object({
+    name: z.string(),
+    registerNumber: z.string(),
+    street: z.string(),
+    number: z.string(),
+    district: z.string(),
+    complement: z.string(),
+    city: z.string(),
+    state: z.string(),
+    postalCode: z.string(),
+  }),
 });
 
 const styles = StyleSheet.create({
@@ -23,34 +71,67 @@ const styles = StyleSheet.create({
     marginTop: 25,
   },
   page: {
-      fontFamily: 'Inter',
-      padding: "32 20 52 20",
+    fontFamily: "Inter",
+    padding: "32 20 52 20",
   },
   label: {
     fontSize: 12,
   },
+  labelSmall: {
+    fontSize: 8,
+  },
+  barCode: {
+    maxWidth: 344,
+    marginTop: 20,
+  },
+  leftColumn: {
+    maxWidth: 399,
+    width: "100%",
+    flexDirection: "column",
+    gap: "4px",
+    padding: "4px 6px",
+  },
+  rightColumn: {
+    flexDirection: "column",
+    gap: "4px",
+    borderLeft: "1px solid black",
+    padding: "4px 6px",
+    height: "100%",
+  },
+  valueSmall: {
+    fontSize: "10px",
+    width: 148,
+    textAlign: "right",
+  },
+  value: {
+    fontSize: "10px",
+    textTransform: "uppercase",
+  },
 });
 
+const mapLogo = {
+  Woovi: <WooviLogo />,
+};
+
+const formatDate = (date: Date): string => {
+  return new Intl.DateTimeFormat("pt-BR").format(date);
+};
 
 export const Boleto = createPdf({
   component: (args) => {
-    const SvgBarcode = boletoBarcodeSvg(args.number);
-    const qrcode = parseHtmlSvgToPdf(
-      renderToString(createQRCodeSVG(args.qrcode))
-    );
+    const SvgBarcode = boletoBarcodeSvg(args.barcodeData);
 
-    const mapLogo = {
-      Woovi: <WooviLogo />,
-      Vercel: <VercelLogo />,
-    };
+    const displayExpirationDay = formatDate(args.expirationDay);
+    const displayDocumentDate = formatDate(args.documentDate);
+    const displayProcessingDate = formatDate(args.processingDate);
 
     return (
       <Document
         author="woovi.com"
         creator="woovi.com"
         producer="woovi.com"
-        subject={args.companyName}
-        title={`Boleto - ${args.companyName}`}
+        subject={"Woovi"}
+        title={"Boleto - Woovi"}
       >
         <Page style={styles.page}>
           <View
@@ -60,33 +141,34 @@ export const Boleto = createPdf({
               gap: 6,
             }}
           >
-            <View
+            <Link
+              href={"https://woovi.com"}
               style={{
-                  width: 100,
+                width: 100,
               }}
             >
-              {mapLogo[args.companyName]}
-            </View>
-              <Text
-                style={{
-                  fontSize: 14,
-                  fontWeight: "bold",
-                  padding: "0 6px",
-                  borderLeft: "1px solid black",
-                  borderRight: "1px solid black",
-                }}
-              >
-                {args.short}
-              </Text>
+              {mapLogo.Woovi}
+            </Link>
+            <Text
+              style={{
+                fontSize: 14,
+                fontWeight: "bold",
+                padding: "0 6px",
+                borderLeft: "1px solid black",
+                borderRight: "1px solid black",
+              }}
+            >
+              {args.documentNumber}
+            </Text>
 
-              <Text
-                style={{
-                  fontSize: 12,
-                  fontWeight: "bold",
-                }}
-              >
-                {args.number}
-              </Text>
+            <Text
+              style={{
+                fontSize: 12,
+                fontWeight: "bold",
+              }}
+            >
+              {args.scannerBarcode}
+            </Text>
           </View>
 
           <View
@@ -96,68 +178,40 @@ export const Boleto = createPdf({
               alignItems: "baseline",
             }}
           >
-            <View
-              style={{
-                maxWidth: 399,
-                flexDirection: "column",
-                gap: "4px",
-                padding: "4px 6px",
-              }}
-            >
-              <Text
-                style={{
-                  fontSize: "8px",
-                }}
-              >
-                Local de pagamento
-              </Text>
-              <Text
-                style={{
-                  fontSize: "10px",
-                  textTransform: "uppercase",
-                }}
-              >
-                Pagável preferencialmente nas agências da Woovi.
-              </Text>
+            <View style={{...styles.leftColumn}}>
+              <Text style={styles.labelSmall}>Local de pagamento</Text>
+              <Text style={styles.value}>{args.paymentPlace}</Text>
             </View>
-            <View
-              style={{
-                flexDirection: "column",
-                gap: "4px",
-                borderLeft: "1px solid black",
-                padding: "4px 6px",
-              }}
-            >
-              <Text
-                style={{
-                  fontSize: "8px",
-                }}
-              >
-                Vencimento
-              </Text>
-              <Text
-                style={{
-                  fontSize: "10px",
-                  width: 148,
-                  textAlign: "right",
-                }}
-              >
-                {args.dueDate}
-              </Text>
+            <View style={styles.rightColumn}>
+              <Text style={styles.labelSmall}>Vencimento</Text>
+              <Text style={styles.valueSmall}>{displayExpirationDay}</Text>
             </View>
           </View>
 
-          <View>
-            <QRCode qrcode={qrcode}  />
-          </View>
           <View
             style={{
-              maxWidth: "344px",
-              margin: "20px",
+              border: "1px solid black",
+              borderTop: "none",
+              flexDirection: "row",
+              alignItems: "baseline",
+              height: '108px'
             }}
           >
-            {SvgBarcode}
+            <View style={{...styles.leftColumn}}>
+              <Text style={styles.labelSmall}>Instruções</Text>
+              <Text style={styles.value}>{args.instructions}</Text>
+            </View>
+            <View style={styles.rightColumn}>
+              <Text style={styles.labelSmall}>(=)Valor Documento</Text>
+              <Text style={styles.valueSmall}>{args.formattedValue}</Text>
+            </View>
           </View>
+
+          <View style={{ marginTop: "10px" }}>
+            <QRCode qrcode={args.qrCodeData} />
+          </View>
+
+          <View style={styles.barCode}>{SvgBarcode}</View>
         </Page>
       </Document>
     );
@@ -166,21 +220,61 @@ export const Boleto = createPdf({
   stories: {
     default: {
       args: {
-        companyName: "Woovi",
-        short: "237-2",
-        number: "23791.11103 60000.000103 01000.222206 1 48622000000000",
-        dueDate: "29/01/2011",
-        qrcode: "https://woovi.com",
-      },
-      prefix: "Basic",
-    },
-    custom: {
-      args: {
-        companyName: "Vercel",
-        short: "237-2",
-        number: "23791.11103 60000.000103 01000.222206 1 48622000000000",
-        dueDate: "29/01/2011",
-        qrcode: "00020126330014BR.GOV.BCB.PIX01110980827191052040000530398654040.015802BR5919Julio Cezar Merisio6009SAO PAULO62140510QISyLSve056304551F",
+        barcodeData: "23797726700000009997506091900000120800542910",
+        scannerBarcode:
+          "23797.50603 91900.000125 08005.429108 7 72670000000999",
+        paymentPlace:
+          "Pagável preferencialmente na rede Bradesco ou Bradesco Expresso.",
+        beneficiary: "UNICRED FLORIANÓPOLIS - CNPJ: 074.064.502/0001-12",
+        beneficiaryAddress:
+          "Rua Tenete Silveira, 315 - Centro - Florianópolis - SC  - CEP 88010-301",
+        instructions:
+          "Após o vencimento cobrar multa de 2,00% , mais juros ao mes de 1,00%.",
+        agency: "7506",
+        agencyDigit: "0",
+        account: "54291",
+        accountDigit: "1",
+        expirationDay: new Date(2017, 7, 30),
+        documentDate: new Date(2017, 7, 18),
+        processingDate: new Date(2017, 7, 18),
+        card: "09",
+        documentNumber: "42493",
+        formattedOurNumber: "09/19000001208-0",
+        formattedValue: "R$ 9,90",
+        documentType: "DS",
+        accept: "N",
+        currencyType: "Real (R$)",
+        amount: " ",
+        valueOf: " ",
+        discountValue: " ",
+        otherDiscounts: " ",
+        feeValue: " ",
+        otherFees: " ",
+        chargeValue: " ",
+        qrCodeData:
+          "00020126580014br.gov.bcb.pix01369e94d622-aecd-4f41-9b0e-5e9342465383520400005303986540510.005802BR5910Flop_Robux6009Sao_Paulo62290525617a30238ffb442e99a6a0c266304F356",
+        payer: {
+          name: "Anita Albuquerque",
+          registerNumber: "221.412.772-05",
+          street: "Rua Maria Gertrudes Coelho",
+          number: "827",
+          complement: " ",
+          district: "Estrada Nova",
+          city: "Divinópolis",
+          state: "MG",
+          postalCode: "35500-700",
+        },
+        guarantor: {
+          name: "ACME Telecomunicações Ltda",
+          registerNumber: "074.064.502/0001-12",
+          street: "Servidão",
+          number: "439",
+          district: "Estrada Nova",
+          complement: " ",
+          city: "Jaraguá do Sul",
+          state: "SC",
+          postalCode: "89254-375",
+        },
       },
       prefix: "Basic",
     },
