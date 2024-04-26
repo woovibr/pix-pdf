c3f97aebe49073c100e18835f61c4ab412390e5d
Julio Merisio <contatomerisio@gmail.com>
2024-04-26 20:54:59 -0300

    fix(fonts): load fonts correctly on build and storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -6,7 +6,7 @@ const config: StorybookConfig = {
     "@storybook/addon-onboarding",
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
   ],
   framework: {
     name: "@storybook/react-vite",
@@ -15,5 +15,6 @@ const config: StorybookConfig = {
   docs: {
     autodocs: "tag",
   },
+  staticDirs: ["../public"],
 };
 export default config;
